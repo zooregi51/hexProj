9602c4105f10f864d948fca0a6faa5efa1c24081
wodnjs395 <kimjw395@gmail.com>
2024-10-28 22:04:50 +0900

    핸들러에 주석 추가중
    jsp 파일 배치

--- src/main/java/salary/command/SalaryLedgerHandler.java
+++ src/main/java/salary/command/SalaryLedgerHandler.java
@@ -12,6 +12,19 @@ import salary.model.SalaryLedgerMonth;
 import salary.service.GetSalaryLedgerService;
 import salary.service.SalaryTransferService;
 
+/*
+ * 급여대장 페이지
+ * /salary/salLedger.do의 요청이 들어오면 처리하는 핸들러
+ * GET 요청 + term parameter가 들어왔을 때 해당 상세 급여대장을 출력하는 서비스를 실행한다.
+ * GET 요청 + year parameter가 들어왔을 때 해당 년도의 달별 급여대장을 출력하는 서비스를 실행한다.
+ * GET 요청만 들어왔을 때 현재 년도를 가져오고 현재 년도의 달별 급여대장을 출력하는 서비스를 실행한다.
+ * 
+ * String year req에 들어있는 year라는 패러미터에서 값을 꺼내옴
+ * String term req에 들어있는 term라는 패러미터에서 값을 꺼내옴
+ * ArrayList<Salary> ledgerDetail 해당 달의 사원들의 상세 급여대장을 DB로부터 저장하는 자료형으로서 view에 ledgerDetail로 세팅
+ * ArrayList<SalaryLedgerMonth> ledgerMonth 해당 달의 급여대장을 DB로부터 저장하는 자료형으로서 view에 ledgerMonth로 세팅
+ */
+
 public class SalaryLedgerHandler implements CommandHandler {
 
 	private GetSalaryLedgerService ledgerSer = new GetSalaryLedgerService();
@@ -33,7 +46,6 @@ public class SalaryLedgerHandler implements CommandHandler {
 			return "/WEB-INF/view/salary/salaryLedgerList.jsp?year=" + year;
 		} else {
 			// 기본값(현재 년도) 급여대장 출력
-			// 급여대장 model에 
 			LocalDate now = LocalDate.now();
 			ArrayList<SalaryLedgerMonth> ledgerMonth = ledgerSer.getYearLedgerMonth(now.getYear());
 			req.setAttribute("ledgerMonth", ledgerMonth);
